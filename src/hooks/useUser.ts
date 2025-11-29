d009112b10205e0425915c22246aa91901edbdbf
JoshuaSolares <joshuavladimir000@gmail.com>
2025-11-29 13:40:08 -0600

    Consumiendo servicio Api Users

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,17 @@
+import type { AppDispath, RootState } from "../store/store";
+import { useDispatch, useSelector } from "react-redux"
+import {userService} from '../services/userService';
+
+
+export const useUser = () => {
+    const {users} = useSelector((state: RootState) => state.users);
+    const dispatch = useDispatch<AppDispath>();
+
+    const getUsers = async () => {
+        const response = await userService.findAll(); 
+    }
+    
+    return {
+        getUsers
+    }
+}   
